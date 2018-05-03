98dcd331280aa30c1dfa044cef2d93f3d9e7a850
Miled Othmen <Miled.Othmen@se.linedata.com>
2018-05-03 13:07:49 +0100

    Add AppSetting.json and Assembly.cs for test report

--- AccountBalance.domain/Events/AccountCreated.cs
+++ AccountBalance.domain/Events/AccountCreated.cs
@@ -2,11 +2,19 @@
 {
     using System;
     using ReactiveDomain.Messaging;
+    using Newtonsoft.Json;
+
 
     public class AccountCreated : Event
     {
         public AccountCreated(CorrelatedMessage source)
             : base(source)
+        { }
+
+        //every event needs that JsonConstructor attributed constructor or it cannot be deserialized from eventstore
+        [JsonConstructor]
+        public AccountCreated(CorrelationId correlationId, SourceId sourceId)
+                    : base(correlationId, sourceId)
         { }
 
         public Guid AccountId { get; set; }
