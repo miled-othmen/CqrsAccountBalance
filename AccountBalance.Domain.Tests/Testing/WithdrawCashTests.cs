5671efd4b0727167506f461cfc3a9f90031bf16d
Miled Othmen <MOthmen@se.linedata.com>
2018-05-06 02:36:55 +0100

    Use case 6

---
+++ AccountBalance.Domain.Tests/Testing/WithdrawCashTests.cs
@@ -0,0 +1,53 @@
+﻿namespace AccountBalance.Domain.Tests
+{
+    using AccountBalance.Domain.Tests.Common;
+    using NodaTime;
+    using ReactiveDomain.Messaging;
+    using System.Threading.Tasks;
+    using Xunit;
+    using Xunit.ScenarioReporting;
+
+    [Collection("AggregateTest")]
+    public class WithdrawCashTests : TestsBase
+    {
+        public WithdrawCashTests(EventStoreFixture fixture) : base(fixture)
+        { }
+
+        [Fact]
+        public async Task CanWithdrawCash()
+        {
+            Clock.SetCurrent(new LocalDateTime(2018, 1, 1, 9, 0));
+
+            var accountCreated = new AccountCreated(CorrelatedMessage.NewRoot())
+            {
+                AccountId = AccountId,
+                AccountHolderName = "NNN"
+            };
+
+            var cashDeposited = new CashDeposited(accountCreated)
+            {
+                AccountId = AccountId,
+                Amount = 350,
+                DepositedAt = Clock.Current
+            };
+
+            var cmd = new WithdrawCash()
+            {
+                AccountId = AccountId,
+                Amount = 200
+            };
+
+            var ev = new CashWithdrawn(cmd)
+            {
+                AccountId = AccountId,
+                Amount = 200,
+                WithdrawnAt = Clock.Current
+            };
+
+            await Runner.Run(
+                def => def.Given(accountCreated, cashDeposited).When(cmd).Then(ev)
+            );
+        }
+
+    }
+}
---
+++ AccountBalance.domain/Commands/WithdrawCash.cs
@@ -0,0 +1,15 @@
+﻿namespace AccountBalance.Domain
+{
+    using NodaTime;
+    using ReactiveDomain.Messaging;
+    using System;
+
+    public class WithdrawCash : Command
+    {
+        public WithdrawCash() : base(NewRoot())
+        { }
+
+        public Guid AccountId { get; set; }
+        public double Amount { get; set; }
+    }
+}
